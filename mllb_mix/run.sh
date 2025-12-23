44ad553babfd740d68bbd261159b03f7bf1bf07c
Dragoonegg <31447168+Dragoonegg@users.noreply.github.com>
2025-12-23 17:38:36 +0800

    Add files via upload

--- mllb_mix/run.sh
+++ mllb_mix/run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
 
 sudo insmod mllb_kern.ko hsaco_path=$(readlink -f ./mllb.hsaco) cubin_path=$(readlink -f ./mllb.cubin)
 sudo rmmod mllb_kern

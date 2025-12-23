334ba764e8edfa608bd27164fa4d9812217f9642
Dragoonegg <31447168+Dragoonegg@users.noreply.github.com>
2025-12-23 17:38:54 +0800

    Add files via upload

--- kml_mix/run.sh
+++ kml_mix/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 sudo insmod kml.ko hsaco_path=$(readlink -f ./kml.hsaco) cubin_path=$(readlink -f ./kml.cubin)
 sudo rmmod kml

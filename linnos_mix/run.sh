20762876378e9ca9b1326ddb42167919d4d9c579
Dragoonegg <31447168+Dragoonegg@users.noreply.github.com>
2025-12-23 17:39:39 +0800

    Add files via upload

--- linnos_mix/run.sh
+++ linnos_mix/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
 sudo insmod linnos.ko cubin_path=$(readlink -f ./linnos.cubin) hsaco_path=$(readlink -f ./linnos.hsaco)
 sudo rmmod linnos
